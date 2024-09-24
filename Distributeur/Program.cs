dac261ad736213b7376391ac94c98ab19a11130b
Samuel Legrain <samuel.legrain@bstorm.be>
2024-09-24 12:15:13 +0200

    Update Program to V3

--- Distributeur/Program.cs
+++ Distributeur/Program.cs
@@ -4,53 +4,39 @@
     {
         static void Main(string[] args)
         {
-            int stock1 = 5; //Valeur fictive pour test
-            int stock2 = 2; //Valeur fictive pour test
-            int stock3 = 0; //Valeur fictive pour test
-            string choix;
-            do
+            int[] stocks = new int[3];
+            stocks[0] = 3; //Valeur fictive pour test
+            stocks[1] = 0; //Valeur fictive pour test
+            stocks[2] = 2; //Valeur fictive pour test
+            string[] noms = new string[3];
+            noms[0] = "Eau";
+            noms[1] = "Soda";
+            noms[2] = "Orangeade";
+            int totalStock = stocks.Count();
+            int choix = 0;
+            while (totalStock > 0 && choix != 4)
             {
-
-                Console.WriteLine("Veuillez sélectionner une boisson :");
-                Console.WriteLine("1. Eau");
-                Console.WriteLine("2. Soda");
-                Console.WriteLine("3. Orangeade");
-                Console.WriteLine("4. Finir");
-
-                choix = Console.ReadLine();
-
-                if (choix == "1")
+                do
                 {
-                    if (stock1 != 0)
+                    Console.WriteLine("Veuillez sélectionner une boisson :");
+                    for (int i = 0; i < noms.Length; i++)
                     {
-                        Console.WriteLine("Voici votre eau");
-                        stock1 = stock1 - 1;
+                        Console.WriteLine($"{i + 1}. {noms[i]}");
+                    }
+                    Console.WriteLine("4. Finir");
+                } while (!int.TryParse(Console.ReadLine(), out choix) || choix < 1 || choix > 4);
+                if (choix != 4)
+                {
+                    if (stocks[choix - 1] > 0)
+                    {
+                        Console.WriteLine($"Voici votre {noms[choix - 1]}");
+                        stocks[choix - 1]--;
                     }
                     else
-                        Console.WriteLine("Sold out!");
-
+                        Console.WriteLine($"Plus de {noms[choix - 1]}");
                 }
-                else if (choix == "2")
-                {
-                    if (stock2 != 0)
-                    {
-                        Console.WriteLine("Voici votre soda");
-                        stock2 = stock2 - 1;
-                    }
-                    else
-                        Console.WriteLine("Sold out!");
-                }
-                else if (choix == "3")
-                {
-                    if (stock3 != 0)
-                    {
-                        Console.WriteLine("Voici votre orangeade");
-                        stock3 = stock3 - 1;
-                    }
-                    else
-                        Console.WriteLine("Sold out!");
-                }
-            } while ((stock1 + stock2 + stock3 > 0) && choix != "4");
+                totalStock = stocks.Count();
+            }
             Console.WriteLine("Merci d'utiliser distributeur 3000!");
         }
     }
